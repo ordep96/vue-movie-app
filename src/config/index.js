c34e68af08111ceb0ff7c28a5dac10575d87e989
Pedro Muñoz <pedro.pmo55@gmail.com>
2020-07-12 20:41:38 -0500

    update package.json

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,13 @@
-export const movieApiKey = process.env['VUE_APP_MOVIE_API_KEY'];
+export const movieApiKey = process.env.VUE_APP_MOVIE_API_KEY;
 export const firebaseConfig = {
-  apiKey: process.env['VUE_APP_FIREBASE_API_KEY'],
-  authDomain: process.env['VUE_APP_FIREBASE_AUTH_DOMAIN'],
-  databaseURL: process.env['VUE_APP_FIREBASE_DATABASE_URL'],
-  projectId: process.env['VUE_APP_FIREBASE_PROJECT_ID'],
-  storageBucket: process.env['VUE_APP_FIREBASE_STORAGE_BUCKET'],
-  messagingSenderId: process.env['VUE_APP_FIREBASE_MESSAGING_SENDER_ID'],
-  appId: process.env['VUE_APP_FIREBASE_APP_ID'],
-  measurementId: process.env['VUE_APP_FIREBASE_MEASUREMENT_ID']
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 }
 
-export const passToKeys = process.env['VUE_APP_PASS_TO_GET_KEYS']
+export const passToKeys = process.env.VUE_APP_PASS_TO_GET_KEYS
